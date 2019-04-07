a9daf2be3d66d9be9687fd86011d94d88c9186d7
Matthew Turk <matthewturk@gmail.com>
2019-04-07 14:19:44 -0500

    Updating the scoping

--- paper-figures.js
+++ paper-figures.js
@@ -41,38 +41,41 @@ var PaperFigures = ( function( Reveal ){
     promise.then( function() {
 
         function disablePaper(slide) {
-            if (!slide.hasAttribute("data-paper-canvas")) return;
-            canvasId = slide.getAttribute("data-paper-canvas");
-            myCanvas = slide.querySelector("canvas#" + canvasId);
-            if (!myCanvas.hasAttribute("data-paper-scope")) {
-                // Seems like it isn't initialized
-                return;
-            }
-            paper.PaperScope.get(myCanvas.getAttribute("data-paper-scope")).view.pause();
+            slide.querySelectorAll("canvas[data-paper-scope]").forEach(function(el) {
+                paper.PaperScope.get(el.getAttribute("data-paper-scope")).view.pause();
+            });
         };
 
+        function loadAndExecute(scriptId, scope) {
+            loadPaperFigure("./" + scriptId + ".js")
+                .then(function(fileData) {
+                    console.log("Loading ", scriptId, scope);
+                    scope.execute(fileData, {'url': "./" + scriptId + ".js", 'source': fileData});
+                    scope.view.play();
+                })
+                .catch(function(xhr) {
+                    console.log("Failed to load: ", xhr);
+                });
+        }
+
         function enablePaper(slide) {
-            if (!slide.hasAttribute("data-paper-canvas")) return;
-            canvasId = slide.getAttribute("data-paper-canvas");
-            myCanvas = slide.querySelector("canvas#" + canvasId);
-            if (!myCanvas.hasAttribute("data-paper-scope")) {
-                scope = new paper.PaperScope();
-                scope.install(myCanvas);
-                scope.setup(myCanvas);
-                myCanvas.setAttribute("data-paper-scope", scope._id);
-                // Now we asynchronously load
-                loadPaperFigure("./" + canvasId + ".js")
-                    .then(function(fileData) {
-                        scope.execute(fileData, {'url': "./" + canvasId + ".js", 'source': fileData});
-                        scope.view.play();
-                    })
-                    .catch(function(xhr) {
-                        console.log("Failed to load: ", xhr);
-                    });
-            } else {
-                paper.PaperScope.get(myCanvas.getAttribute("data-paper-scope")).view.play();
-            }
+            slide.querySelectorAll("canvas[data-paper-script]").forEach(function(el) {
+                scriptId = el.getAttribute("data-paper-script");
+                console.log(el);
+                if (!el.hasAttribute("data-paper-scope")) {
+                    scope = new paper.PaperScope();
+                    scope.install(el);
+                    scope.setup(el);
+                    el.setAttribute("data-paper-scope", scope._id);
+                    // Now we asynchronously load, which we do in a separate
+                    // function for scoping purposes
+                    loadAndExecute(scriptId, scope);
+                } else {
+                    paper.PaperScope.get(el.getAttribute("data-paper-scope")).view.play();
+                }
+            });
         };
+
         Reveal.addEventListener( 'slidechanged', function(event) {
             disablePaper(event.previousSlide);
             enablePaper(event.currentSlide);
